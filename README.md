224afc2670ebd3cfad5c2d176c2eb5abaf49ce41
devgioele <gioeledevittidev@gmail.com>
2022-04-02 17:04:41 +0200

    Add basic project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Pre-configured React app for new projects
+
+Features:
+* Create-react-app
+* Typescript
+* ESLint
+* Prettier
+* VS Code settings
---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexFlow: 'column nowrap',
+        justifyContent: 'center',
+        color: '#ffffff',
+        textAlign: 'center',
+        height: '100%',
+      }}
+    >
+      <header
+        style={{
+          flex: '1',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <p style={{ fontSize: 'xx-large' }}>Love and create!🚀</p>
+      </header>
+      <main style={{ flex: '8 0 auto', padding: '2%', paddingTop: '0px' }}>
+        <a
+          style={{ fontSize: 'x-large' }}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </main>
+    </div>
+  );
+}
